4632790a9bc51f9551b591347697a56b77e3935d
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-01-24 15:11:00 -0300

    Criação das classes de conexão com o banco Cassandra e dos projetos restantes

---
+++ MsNetflixTitles.CrossCutting/CassandraContext.cs
@@ -0,0 +1,36 @@
+﻿using Cassandra;
+using Cassandra.Mapping;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MsNetflixTitles.CrossCutting
+{
+    public class CassandraContext : ICassandraContext
+    {
+        private readonly Cluster _cluster;
+        private readonly ISession _session;
+        private readonly IMapper _mapper;
+
+        public CassandraContext(IConfiguration configuration)
+        {
+            _cluster = Cluster.Builder()
+                            .AddContactPoints(configuration.GetSection("hostCassandra").Value)
+                            .Build();
+
+            _session = _cluster.Connect(configuration.GetSection("keyspaceCassandra").Value);
+
+            _mapper = new Mapper(_session);
+        }
+
+        public async Task<T> FirstOrDefaultAsync<T>(string cqlQuery, params object[] args) => await _mapper.FirstOrDefaultAsync<T>(cqlQuery, args);
+
+        public async Task<IEnumerable<T>> SelectAsync<T>(string cqlQuery, params object[] args) => await _mapper.FetchAsync<T>(cqlQuery, args);
+
+        public async Task InsertAsync<T>(T obj) => await _mapper.InsertAsync<T>(obj);
+
+        public async Task UpdateAsync<T>(T obj) => await _mapper.UpdateAsync<T>(obj);
+
+        public async Task DeleteAsync<T>(T obj) => await _mapper.DeleteAsync<T>(obj);
+    }
+}
---
+++ MsNetflixTitles.CrossCutting/ICassandraContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MsNetflixTitles.CrossCutting
+{
+    public interface ICassandraContext
+    {
+        Task DeleteAsync<T>(T obj);
+
+        Task UpdateAsync<T>(T obj);
+
+        Task InsertAsync<T>(T obj);
+
+        Task<IEnumerable<T>> SelectAsync<T>(string cqlQuery, params object[] args);
+
+        Task<T> FirstOrDefaultAsync<T>(string cqlQuery, params object[] args);
+    }
+}
